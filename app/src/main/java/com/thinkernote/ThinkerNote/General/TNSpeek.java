66df4c297fd75556b18130a184b29c92face4839
JackSong <sjy0118atsn@163.com>
2019-09-17 15:58:23 +0800

    语音转文字 10s自动结束

--- app/src/main/java/com/thinkernote/ThinkerNote/General/TNSpeek.java
+++ app/src/main/java/com/thinkernote/ThinkerNote/General/TNSpeek.java
@@ -139,11 +139,11 @@ public class TNSpeek {
 
         // 设置语音前端点:静音超时时间，即用户多长时间不说话则当做超时处理
 //        mIat.setParameter(SpeechConstant.VAD_BOS, mSharedPreferences.getString("iat_vadbos_preference", "6000"));
-        mIat.setParameter(SpeechConstant.VAD_BOS, "6000");
+        mIat.setParameter(SpeechConstant.VAD_BOS, "10000");
 
         // 设置语音后端点:后端点静音检测时间，即用户停止说话多长时间内即认为不再输入， 自动停止录音
 //        mIat.setParameter(SpeechConstant.VAD_EOS, mSharedPreferences.getString("iat_vadeos_preference", "3500"));
-        mIat.setParameter(SpeechConstant.VAD_EOS, "5000");
+        mIat.setParameter(SpeechConstant.VAD_EOS, "10000");
 
         // 设置标点符号,设置为"0"返回结果无标点,设置为"1"返回结果有标点
 //        mIat.setParameter(SpeechConstant.ASR_PTT, mSharedPreferences.getString("iat_punc_preference", "1"));
@@ -259,7 +259,6 @@ public class TNSpeek {
         }
         return resultBuffer;
     }
-
 
 
     //========================似有方法============================
